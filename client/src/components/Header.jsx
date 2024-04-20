a5594ecebe3dfbedb333c69b7bed0c4b14938613
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-04-20 23:37:39 +0530

    heading

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,11 +5,11 @@ import { useSelector } from 'react-redux';
 const Header = () => {
     const { currentUser } = useSelector((state) => state.user);
     return (
-        <div className='bg-black text-white'>
-            <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <div className='text-white bg-black'>
+            <div className='flex items-center justify-between max-w-6xl p-3 mx-auto'>
                 <Link to='/'>
                     <h1 className='font-bold'>
-                        Authentication App
+                    Authentication with paypal integration App
                     </h1>
                 </Link>
                 <ul className='flex gap-4'>
@@ -21,7 +21,7 @@ const Header = () => {
                     </Link>
                     <Link to='/profile'>
                     {currentUser ? (
-                    <img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
+                    <img src={currentUser.profilePicture} alt='profile' className='object-cover rounded-full h-7 w-7' />
                     ) : (
                     <li>Sign In</li>
                     )}
